c51b30ed39eb0d2657acc2b937c1518e1d8758e4
muhammed alaa <sailorjs2020@gmail.com>
2020-10-13 21:39:27 +0200

    Updates

---
+++ precache-manifest.1c1788d0a39c12c91781a3806b114909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d1af2f072f183adc5115572fdd77ff",
+    "url": "/alaanofear-monsters/index.html"
+  },
+  {
+    "revision": "e057b0e819abe921aa81",
+    "url": "/alaanofear-monsters/static/css/main.64f4a70c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/alaanofear-monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alaanofear-monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e057b0e819abe921aa81",
+    "url": "/alaanofear-monsters/static/js/main.b7f41a2b.chunk.js"
+  },
+  {
+    "revision": "afd23207b858569c5bc3",
+    "url": "/alaanofear-monsters/static/js/runtime-main.5e800fc0.js"
+  }
+]);
